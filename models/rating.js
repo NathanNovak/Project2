a9c8891d64c1146de44dfc5fc44939dffbe91e31
Nathan Novak <nnovak@ku.edu>
2018-08-09 21:19:39 -0700

    set up DB and started Welcome page

---
+++ models/rating.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+  var Rating = sequelize.define("Rating", {
+    rating: DataTypes.INTEGER
+  });
+
+  Rating.associate = function(models) {
+    Rating.belongsTo(models.Beer, {
+      as: "Beer",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    Rating.belongsTo(models.Users, {
+      as: "Users ",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Rating;
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,42 +1,44 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
+var $nameText = $("#name-text");
+var $ageText = $("#age-text");
+var $emailText = $("#email-text");
+// var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function(UserInfo) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
+      url: "api/Users",
+      data: JSON.stringify(UserInfo)
     });
   },
   getExamples: function() {
     return $.ajax({
-      url: "api/examples",
+      url: "api/Users",
       type: "GET"
     });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
   }
+  // deleteExample: function(id) {
+  //   return $.ajax({
+  //     url: "api/examples/" + id,
+  //     type: "DELETE"
+  //   });
+  // }
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
 var refreshExamples = function() {
   API.getExamples().then(function(data) {
     var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+      // var $a = $("<a>")
+      //   .text(example.text)
+      //   .attr("href", "/example/" + example.id);
 
       var $li = $("<li>")
         .attr({
@@ -64,36 +66,40 @@ var refreshExamples = function() {
 var handleFormSubmit = function(event) {
   event.preventDefault();
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  var UserInfo = {
+    name: $nameText.val().trim(),
+    age: $ageText.val().trim(),
+    email: $emailText.val().trim()
   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
+  if (!(UserInfo.name && UserInfo.age)) {
+    alert("You must enter your name and age so we know you can be here!");
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(UserInfo).then(function() {
     refreshExamples();
   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
+
+  $nameText.val("");
+  $ageText.val("");
+  $emailText.val("");
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+// var handleDeleteBtnClick = function() {
+//   var idToDelete = $(this)
+//     .parent()
+//     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
+//   API.deleteExample(idToDelete).then(function() {
+//     refreshExamples();
+//   });
+// };
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+console.log($nameText, $ageText, $emailText);
+// $exampleList.on("click", ".delete", handleDeleteBtnClick);
